a5cfeb546ce0a46559060bd33b6d4028e361ca29
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-10-04 13:49:53 +0300

    add app plyr

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import Plyr, { APITypes } from "plyr-react";
+import "plyr-react/plyr.css";
+
+const videoId = "yWtFb9LJs3o";
+const provider = "youtube";
+const videoOptions = undefined;
+
+const PlyrComponent = () => {
+  const ref = useRef<APITypes>(null);
+
+  const enterVideo = () => {
+    (ref.current?.plyr as Plyr)?.fullscreen.enter();
+  };
+
+  const plyrVideo =
+    videoId && provider ? (
+      <Plyr
+        ref={ref}
+        source={{
+          type: "video",
+          sources: [
+            {
+              src: videoId,
+              provider: provider
+            }
+          ]
+        }}
+        options={videoOptions}
+      />
+    ) : null;
+
+  return (
+    <div className="player__wrapper">
+      {plyrVideo}
+      <button onClick={enterVideo}>fullscreen</button>
+    </div>
+  );
+};
+
+export default PlyrComponent;
